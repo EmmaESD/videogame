53e4a90cf66984c7f48996e45b3b54bc83a0dd49
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-06-10 15:54:19 +0200

    add jump function

--- player.gd
+++ player.gd
@@ -4,6 +4,9 @@ extends CharacterBody3D
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
+#...
+# Vertical impulse applied to the character upon jumping in meters per second.
+@export var jump_impulse = 20
 
 var target_velocity = Vector3.ZERO
 
@@ -34,4 +37,7 @@ func _physics_process(delta):
 
 	# Moving the Character
 	velocity = target_velocity
+	# Jumping.
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
 	move_and_slide()
